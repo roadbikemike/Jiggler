3027d43e7415cb9ab5511c579b69d302e998b225
Mike Thompson <mike.thompson@ticketmaster.com>
2026-01-24 08:38:27 -0500

    ...

--- User_Setup.h
+++ User_Setup.h
@@ -1,9 +1,13 @@
 // User setup for ST7789 240x135 TFT (HiLetgo ESP32 LCD)
 #define ST7789_DRIVER
-#define CGRAM_OFFSET        // CRITICAL: Fix pixel addressing for TTGO T-Display
+// #define CGRAM_OFFSET        // Removed - may be causing addressing issues
 #define TFT_WIDTH 240
 #define TFT_HEIGHT 135
 #define TFT_RGB_ORDER TFT_BGR  // BGR color order for TTGO T-Display
+
+// Display offsets for TTGO T-Display (physical display is 240x280, visible is 240x135)
+#define TFT_COL_OFFSET 40
+#define TFT_ROW_OFFSET 52
 #define TFT_MOSI 19
 #define TFT_SCLK 18
 #define TFT_CS 5
@@ -14,4 +18,4 @@
 #define SPI_FREQUENCY 27000000
 #define SPI_READ_FREQUENCY 20000000
 #define SPI_TOUCH_FREQUENCY 2500000
-#define TFT_INVERSION_OFF    // Try OFF instead - some TTGO variants need this
+#define TFT_INVERSION_ON    // Most TTGO T-Display boards need inversion ON
